f2b5df49dfe742ea6a7de10411f9773748762811
dev <doradomiguel35@gmail.com>
2019-04-23 13:55:04 +0800

    can now register users

--- django_angular/settings.py
+++ django_angular/settings.py
@@ -49,7 +49,10 @@ REST_FRAMEWORK = {
     )
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
